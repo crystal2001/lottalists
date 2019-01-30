6a3333d60826d01b7b936c4b391898d041a89236
crystal2001 <43784510+crystal2001@users.noreply.github.com>
2019-01-30 12:41:35 -0500

    1

---
+++ classlist.py
@@ -0,0 +1,7 @@
+import time
+classes = ['english','math','history','science','gym','health','advisory','production']
+
+for subject in classes:
+	print('You have been enrolled in ' + subject)
+	print()
+	time.sleep(0.5)
---
+++ numberslist.py
@@ -0,0 +1,8 @@
+import time
+
+numbers = [3,5,9,17,123,666,1000,4321]
+
+for number in numbers:
+	print(2 * number)
+	print()
+	time.sleep(0.5)
